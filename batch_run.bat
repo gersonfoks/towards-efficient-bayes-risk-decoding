d3ee2d289195cba00bb186ec3c9f29845263a43b
gersonfoks <gerson.foks@gmail.com>
2022-07-21 22:58:29 +0200

    some change

--- batch_run.bat
+++ batch_run.bat
@@ -3,8 +3,8 @@
 
 
 
-python -m scripts.hyperparamsearch --utility=comet --model-name=last_hidden_state_lstm_model --smoke-test
-python -m scripts.hyperparamsearch --utility=comet --model-name=last_hidden_state_attention_model --smoke-test
+python -m scripts.hyperparamsearch --utility=comet --model-name=last_hidden_state_lstm_model
+python -m scripts.hyperparamsearch --utility=comet --model-name=last_hidden_state_attention_model
 
 
 
