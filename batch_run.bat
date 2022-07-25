1bba5e8e9faf234ac35faaa5bd5362f8997b6877
gersonfoks <gerson.foks@gmail.com>
2022-07-25 11:25:44 +0200

    changed batch run

--- batch_run.bat
+++ batch_run.bat
@@ -1,9 +1,5 @@
 
 
-
-
-
-python -m scripts.hyperparamsearch --utility=comet --model-name=token_statistics_lstm
 python -m scripts.hyperparamsearch --utility=comet --model-name=token_statistics_attention
 
 
--- batch_run_2.bat
+++
@@ -1,6 +0,0 @@
-
-
-python -m scripts.hyperparamsearch --model-name=last_hidden_state_attention_model
-
-
-
