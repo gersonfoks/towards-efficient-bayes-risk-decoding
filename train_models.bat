a4a8b6f40fb5cf282ea8a248a6138ab5254aedf5
gerson.foks <gerson.foks@gmail.com>
2022-08-12 11:06:12 +0200

    adds a new bat file

---
+++ train_models.bat
@@ -0,0 +1,8 @@
+
+
+
+
+python -m scripts.models.basic_model.train_model
+python -m scripts.models.last_hidden_state_model.train_model
+python -m scripts.models.token_statistics_model.train_model
+python -m scripts.models.full_dec_model.train_model
