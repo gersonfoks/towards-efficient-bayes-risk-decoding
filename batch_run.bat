f53e1ab417a7815889e12f4344ceebf402dc5893
gersonfoks <gerson.foks@gmail.com>
2022-08-15 10:51:45 +0200

    small change

--- batch_run.bat
+++ batch_run.bat
@@ -1,6 +1,4 @@
 
-
-
 python -m scripts.preperation.generate_data --split=test --n-samples=100 --seed=0
 python -m scripts.preperation.generate_data --split=test --n-samples=1000 --seed=0
 python -m scripts.preperation.create_bayes_risk_dataset --n-hypotheses=100 --n-references=1000 --split=test
