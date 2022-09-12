7022ee0b089b00a647af36b04762cce72efa08bf
gersonfoks <gerson.foks@gmail.com>
2022-09-12 10:18:33 +0200

    added scripts for training

--- train_models.bat
+++ train_models.bat
@@ -1,13 +1,10 @@
 
 
+python -m scripts.models.comet_model.train_model
 
 
-python -m scripts.models.basic_reference_model.train_model --config=./configs/predictive/comet/basic_reference_model.yml --n-references=1
-python -m scripts.models.basic_reference_model.train_model --config=./configs/predictive/comet/basic_reference_model.yml --n-references=2
-python -m scripts.models.basic_reference_model.train_model --config=./configs/predictive/comet/basic_reference_model.yml --n-references=3
-python -m scripts.models.basic_reference_model.train_model --config=./configs/predictive/comet/basic_reference_model.yml --n-references=4
-python -m scripts.models.basic_reference_model.train_model --config=./configs/predictive/comet/basic_reference_model.yml --n-references=5
-python -m scripts.models.basic_reference_model.train_model --config=./configs/predictive/comet/basic_reference_model.yml --n-references=10
-python -m scripts.models.basic_reference_model.train_model --config=./configs/predictive/comet/basic_reference_model.yml --n-references=25
-python -m scripts.models.basic_reference_model.train_model --config=./configs/predictive/comet/basic_reference_model.yml --n-references=50
-python -m scripts.models.basic_reference_model.train_model --config=./configs/predictive/comet/basic_reference_model.yml --n-references=100
+python -m scripts.models.unigram_count_model.train_model --config=./configs/predictive/unigramf1/unigram_count_model.yml --n-references=1
+python -m scripts.models.unigram_count_model.train_model --config=./configs/predictive/unigramf1/unigram_count_model.yml --n-references=2
+python -m scripts.models.unigram_count_model.train_model --config=./configs/predictive/unigramf1/unigram_count_model.yml --n-references=3
+python -m scripts.models.unigram_count_model.train_model --config=./configs/predictive/unigramf1/unigram_count_model.yml --n-references=4
+python -m scripts.models.unigram_count_model.train_model --config=./configs/predictive/unigramf1/unigram_count_model.yml --n-references=5
