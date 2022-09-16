5c3d7a5faebef8f90405e5e4fbed0615b30ce650
gerson.foks <gerson.foks@gmail.com>
2022-09-16 14:36:49 +0200

    finished full dec comet model study

--- train_models.bat
+++ train_models.bat
@@ -1,9 +1,5 @@
 
 
-
-python -m scripts.models.basic_model.train_model --config=./configs/predictive/chrf/basic_model.yml --utility=chrf
-python -m scripts.models.last_hidden_state_model.train_model --config=./configs/predictive/chrf/last_hidden_state_model.yml --utility=chrf
-python -m scripts.models.token_statistics_model.train_model --config=./configs/predictive/chrf/token_statistics_model.yml --utility=chrf
 python -m scripts.models.full_dec_model.train_model --config=./configs/predictive/chrf/full_dec_model.yml --utility=chrf
 python -m scripts.models.full_dec_no_stat_model.train_model --config=./configs/predictive/chrf/full_dec_no_stat_model.yml --utility=chrf
 python -m scripts.models.comet_model.train_model --config=./configs/predictive/chrf/comet_model.yml --utility=chrf
