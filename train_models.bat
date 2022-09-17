79e4c982824b83d4060d158b122687b88bac2432
gerson.foks <gerson.foks@gmail.com>
2022-09-17 17:37:55 +0200

    trained full dec comet model

--- train_models.bat
+++ train_models.bat
@@ -1,12 +1,9 @@
 
 
-python -m scripts.models.full_dec_model.train_model --config=./configs/predictive/chrf/full_dec_model.yml --utility=chrf
-python -m scripts.models.full_dec_no_stat_model.train_model --config=./configs/predictive/chrf/full_dec_no_stat_model.yml --utility=chrf
-python -m scripts.models.comet_model.train_model --config=./configs/predictive/chrf/comet_model.yml --utility=chrf
+python -m scripts.models.full_dec_comet_model.train_model --config=./configs/predictive/chrf/full_dec_comet_model.yml --utility=chrf
 
+python -m scripts.models.full_dec_comet_model.train_model --config=./configs/predictive/comet/full_dec_comet_model.yml --utility=comet
 
+python -m scripts.models.full_dec_comet_model.train_model --config=./configs/predictive/unigramf1/full_dec_comet_model.yml --utility=unigram-f1
 
-
-
-python -m scripts.models.comet_model.train_model --config=./configs/predictive/unigramf1/comet_model.yml --utility=unigram-f1
 
